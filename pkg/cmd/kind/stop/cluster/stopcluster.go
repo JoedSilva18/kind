405a978ec527eadd2c43c0abf88858ad89ad0bff
Joed Silva <joedsilva2016@gmail.com>
2022-12-04 21:51:59 -0300

    adicionando prototipo do comando de stop

---
+++ pkg/cmd/kind/stop/cluster/stopcluster.go
@@ -0,0 +1,80 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package cluster implements the `stop` command
+package cluster
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os/exec"
+	"sigs.k8s.io/kind/pkg/cluster"
+	"sigs.k8s.io/kind/pkg/cmd"
+	"sigs.k8s.io/kind/pkg/internal/cli"
+	"sigs.k8s.io/kind/pkg/log"
+	"time"
+)
+
+type flagpole struct {
+	Name       string
+	Config     string
+	ImageName  string
+	Retain     bool
+	Wait       time.Duration
+	Kubeconfig string
+}
+
+// NewCommand to kill one or more running containers
+func NewCommand(logger log.Logger, streams cmd.IOStreams) *cobra.Command {
+	flags := &flagpole{}
+	cmd := &cobra.Command{
+		Args:  cobra.NoArgs,
+		Use:   "container",
+		Short: "Stop one or more running containers",
+		Long:  "Stop one or more running containers",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cli.OverrideDefaultName(cmd.Flags())
+			return runE(logger, streams, flags)
+		},
+	}
+	cmd.Flags().StringVarP(
+		&flags.Name,
+		"name",
+		"n",
+		cluster.DefaultName,
+		"the cluster name",
+	)
+	cmd.Flags().StringVar(
+		&flags.Kubeconfig,
+		"kubeconfig",
+		"",
+		"sets kubeconfig path instead of $KUBECONFIG or $HOME/.kube/config",
+	)
+	return cmd
+}
+
+func runE(logger log.Logger, streams cmd.IOStreams, flags *flagpole) error {
+
+	cmd, err := exec.Command("bash", "-c", "docker kill " + flags.Name).Output()
+
+	if err != nil {
+		logger.Warn(fmt.Sprint(err))
+	}
+
+	logger.V(0).Infof(string(cmd))
+
+	return nil
+}
